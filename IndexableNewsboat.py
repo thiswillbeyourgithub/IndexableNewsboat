ebfa6ad6b2ac628b30ad0c36041eeb19deb864af
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-07-26 21:19:41 +0200

    fix: removes old file

--- IndexableNewsboat.py
+++ IndexableNewsboat.py
@@ -117,6 +117,7 @@ for i in tqdm(db.index):
     save_entry_as_file(i)
 
 print("Compressing as a zip archive...")
+os.system(f"rm -r {args['output_dir']}/IndexableNewsboat.zip")
 os.system(f"cd /tmp/IndexableNewsboat && zip -9 {args['output_dir']}/IndexableNewsboat.zip ./*")
 
 print("Cleaning up...")
